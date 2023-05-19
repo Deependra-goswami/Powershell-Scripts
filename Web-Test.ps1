b703df3aca079e798a4e223dce37083e9ed5c43e
Deependra Goswami <64826265+Deependra-goswami@users.noreply.github.com>
2023-05-20 01:14:24 +1200

    Add files via upload

---
+++ Web-Test.ps1
@@ -0,0 +1,25 @@
+﻿$websites = @(
+    "https://www.sky.co.nz",
+    "https://broadband.sky.co.nz",
+    "https://account.sky.co.nz",
+    "https://get.sky.co.nz",
+    "https://www.primetv.co.nz",
+    "https://www.skybusiness.co.nz",
+    "https://www.skybusiness.co.nz",
+    "https://www.skyadvertising.co.nz"
+)
+foreach ($website in $websites) {
+    $HTTP_Request = [System.Net.WebRequest]::Create($website)
+    $HTTP_Response = $HTTP_Request.GetResponse()
+    $HTTP_Status = [int]$HTTP_Response.StatusCode
+
+    If ($HTTP_Status -eq 200) {
+    Write-Host "`n Site $website is OK!"
+    }
+    Else {
+    Write-Host "`n The Site $website may be down, please check!"
+    }
+ }
+Write-Host "`n"
+Read-Host -Prompt "Press Enter to exit"
+
